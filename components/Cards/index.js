b9e82f789f24abfb1169dc6a43ff6be7a0015d3d
Chris <caquinn.86@gmail.com>
2019-09-13 19:35:31 -0700

    finished articles cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+const getArticles = async function(url) {
+    let res = await axios.get(url);
+    Object.entries(res.data.articles).forEach(element => {
+        element[1].forEach(e => {
+            cardContainer.appendChild(createCard(e));
+        })
+    })
+}
+
+getArticles('https://lambda-times-backend.herokuapp.com/articles');
+
+let create = element => {
+    return document.createElement(element);
+}
+
+function createCard (data) {
+    let card = create('div');
+    let cardHeadline = create('div');
+    let cardAuthor = create('div');
+    let cardImgCont = create('div');
+    let cardImage = create('img');
+    let cardBy = create('span');
+
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+    cardAuthor.appendChild(cardImgCont);
+    cardImgCont.appendChild(cardImage);
+    cardAuthor.appendChild(cardBy);
+
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    cardImgCont.classList.add('img-container');
+
+    cardHeadline.textContent = data.headline;
+    cardBy.textContent = `By ${data.authorName}`;
+    cardImage.setAttribute('src', data.authorPhoto);
+    
+    return card;
+}
